b4eabdf0c7779838223f192631e12e0d523d1126
Hsia2018 <631853070@qq.com>
2019-08-08 14:56:08 +0800

    lenovo commit3

--- seckill-main/src/main/java/com/hsiait/hsiaer/goodsmanage/mapper/GoodsMapper.java
+++ seckill-main/src/main/java/com/hsiait/hsiaer/goodsmanage/mapper/GoodsMapper.java
@@ -19,15 +19,15 @@ import java.util.List;
 public interface GoodsMapper {
 
     //@Select("select g.*,sk.stock_count, sk.start_time, sk.end_time,sk.seckill_price from seckill_goods sk left join goods g on sk.goods_id = g.id")
-    public List<GoodsVo> listGoodsVo();
+    List<GoodsVo> listGoodsVo();
 
     //@Select("select g.*,sk.stock_count, sk.start_time, sk.end_time,sk.seckill_price from seckill_goods sk left join goods g on sk.goods_id = g.id where g.id = #{goodsId}")
-    public GoodsVo getGoodsVoByGoodsId(@Param("goodsId")long goodsId);
+    GoodsVo getGoodsVoByGoodsId(@Param("goodsId")long goodsId);
 
     //@Update("update seckill_goods set stock_count = stock_count - 1 where goods_id = #{goodsId} and stock_count > 0")
-    public int reduceStock(SeckillGoods skg);
+    int reduceStock(SeckillGoods skg);
 
     //@Update("update seckill_goods set stock_count = #{stockCount} where goods_id = #{goodsId}")
-    public int resetStock(SeckillGoods skg);
+    int resetStock(SeckillGoods skg);
 
 }
