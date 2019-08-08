2348a5524fe62b7c474ff069cd2b26a03b37ed6f
Hsia2018 <631853070@qq.com>
2019-08-08 14:06:13 +0800

    lenovo commit2

--- seckill-main/src/main/java/com/hsiait/hsiaer/goodsmanage/mapper/GoodsMapper.java
+++ seckill-main/src/main/java/com/hsiait/hsiaer/goodsmanage/mapper/GoodsMapper.java
@@ -18,16 +18,16 @@ import java.util.List;
 @Repository
 public interface GoodsMapper {
 
-    @Select("select g.*,sk.stock_count, sk.start_time, sk.end_time,sk.seckill_price from seckill_goods sk left join goods g on sk.goods_id = g.id")
+    //@Select("select g.*,sk.stock_count, sk.start_time, sk.end_time,sk.seckill_price from seckill_goods sk left join goods g on sk.goods_id = g.id")
     public List<GoodsVo> listGoodsVo();
 
-    @Select("select g.*,sk.stock_count, sk.start_time, sk.end_time,sk.seckill_price from seckill_goods sk left join goods g on sk.goods_id = g.id where g.id = #{goodsId}")
+    //@Select("select g.*,sk.stock_count, sk.start_time, sk.end_time,sk.seckill_price from seckill_goods sk left join goods g on sk.goods_id = g.id where g.id = #{goodsId}")
     public GoodsVo getGoodsVoByGoodsId(@Param("goodsId")long goodsId);
 
-    @Update("update seckill_goods set stock_count = stock_count - 1 where goods_id = #{goodsId} and stock_count > 0")
+    //@Update("update seckill_goods set stock_count = stock_count - 1 where goods_id = #{goodsId} and stock_count > 0")
     public int reduceStock(SeckillGoods skg);
 
-    @Update("update seckill_goods set stock_count = #{stockCount} where goods_id = #{goodsId}")
+    //@Update("update seckill_goods set stock_count = #{stockCount} where goods_id = #{goodsId}")
     public int resetStock(SeckillGoods skg);
 
 }
