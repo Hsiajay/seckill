a8c51553f26b667addf9bb300aa806e459dd9521
Hsia2018 <631853070@qq.com>
2019-08-13 15:47:37 +0800

    lenovo commit6

--- seckill-main/src/main/java/com/hsiait/hsiaer/rabbitmq/RabbitMQReceiver.java
+++ seckill-main/src/main/java/com/hsiait/hsiaer/rabbitmq/RabbitMQReceiver.java
@@ -56,26 +56,25 @@ public class RabbitMQReceiver {
         seckillService.seckill(user, goods);
     }
 
-    //		@RabbitListener(queues=MQConfig.QUEUE)
-    //		public void receive(String message) {
-    //			log.info("receive message:"+message);
-    //		}
-    //
-    //		@RabbitListener(queues=MQConfig.TOPIC_QUEUE1)
-    //		public void receiveTopic1(String message) {
-    //			log.info(" topic  queue1 message:"+message);
-    //		}
-    //
-    //		@RabbitListener(queues=MQConfig.TOPIC_QUEUE2)
-    //		public void receiveTopic2(String message) {
-    //			log.info(" topic  queue2 message:"+message);
-    //		}
-    //
-    //		@RabbitListener(queues=MQConfig.HEADER_QUEUE)
-    //		public void receiveHeaderQueue(byte[] message) {
-    //			log.info(" header  queue message:"+new String(message));
-    //		}
-    //
+    /*@RabbitListener(queues=RabbitMQConfig.QUEUE)
+    public void receive(String message) {
+        log.info("receive message:"+message);
+    }
+
+    @RabbitListener(queues=RabbitMQConfig.TOPIC_QUEUE1)
+    public void receiveTopic1(String message) {
+        log.info(" topic  queue1 message:"+message);
+    }
+
+    @RabbitListener(queues=RabbitMQConfig.TOPIC_QUEUE2)
+    public void receiveTopic2(String message) {
+        log.info(" topic  queue2 message:"+message);
+    }
+
+    @RabbitListener(queues=RabbitMQConfig.HEADER_QUEUE)
+    public void receiveHeaderQueue(byte[] message) {
+        log.info(" header  queue message:"+new String(message));
+    }*/
 
 
 }
